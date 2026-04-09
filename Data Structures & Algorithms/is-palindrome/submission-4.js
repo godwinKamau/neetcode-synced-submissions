09aa667a67a988c305947d522566d138e99a5b18
Godwin Kamau <contactgodwinkamau@gmail.com>
2026-04-09 15:40:00 -0400

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        
+        let l = 0
+        
+        const regex = /\W+/g
+        const string = s.replace(regex,'').toLowerCase()
+        
+        let r = string.length-1
+
+        while (l < r) {
+            if (string[l] !== string[r]){
+                return false
+            }
+            l++
+            r--
+        }
+        return true
+    }
+}
