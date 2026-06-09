eb53f88a776f1f4e49da6aeff8b1b4aca794a232
Godwin Kamau <contactgodwinkamau@gmail.com>
2026-06-09 11:51:23 -0400

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let map = new Map()
+        for (let i = 0; i < nums.length; i++){
+            const diff = target - nums[i]
+            if (map.has(diff)){
+                
+                return [nums.indexOf(diff),i]
+            } else {
+                map.set(nums[i], i)
+            }
+        }
+    }
+}
