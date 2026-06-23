932572fd100e33633722019a30c965ed9d81407c
Godwin Kamau <contactgodwinkamau@gmail.com>
2026-06-23 17:35:41 -0400

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head) {
+        let curr = head;
+        const set = new Set;
+
+        while (curr) {
+            if (set.has(curr.next)){
+                return true
+            }
+            set.add(curr.next)
+            curr = curr.next
+        }
+
+        return false
+    }
+}
